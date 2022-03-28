5dad0806347984190ab4cb36f244ee6448470fd3
nick-gansta <shuminskiy.nik@mail.ru>
2022-03-28 22:24:25 +0300

    11

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import style from "./app.module.scss";
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
+    <div className={style.App}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
+
+
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+
+
         >
           Learn React
         </a>
