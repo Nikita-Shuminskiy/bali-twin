7f2b5d65e9c5c0d8897bbe37dd98158fee3f683e
nick-gansta <shuminskiy.nik@mail.ru>
2022-03-29 15:24:28 +0300

    fix layout in HomePage.tsx

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import logoHome from '../../Images/Logo-home.png'
 import { AppHeader } from '../AppHeader/AppHeader'
 import styles from './HomePage.module.scss'
 
@@ -6,6 +7,13 @@ export const HomePage = () => {
     return (
         <div className={styles['container']}>
             <AppHeader />
+            <div className={styles['background-pink']} />
+            <div className={styles['background-blue']} />
+            <div className={styles['logo-block']}>
+                <div className={styles['back-top']} />
+                <img src={logoHome} alt="logo-home" />
+                <div className={styles['back-down']} />
+            </div>
         </div>
     )
 }
