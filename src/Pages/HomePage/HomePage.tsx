06727634e022fca8a575e61a2593e48943235fae
nick-gansta <shuminskiy.nik@mail.ru>
2022-03-29 19:09:12 +0300

    create btn on HomePage.tsx

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Btn from '../../Pages/btn'
 import logoHome from '../../Images/Logo-home.png'
 import { AppHeader } from '../AppHeader/AppHeader'
 import styles from './HomePage.module.scss'
@@ -10,9 +11,13 @@ export const HomePage = () => {
             <div className={styles['background-pink']} />
             <div className={styles['background-blue']} />
             <div className={styles['logo-block']}>
-                <div className={styles['back-top']} />
-                <img src={logoHome} alt="logo-home" />
-                <div className={styles['back-down']} />
+                <div>
+                    <div className={styles['back-top']} />
+                    <img src={logoHome} alt="logo-home" />
+                    <div className={styles['back-down']} />
+                </div>
+                <Btn />
+                <div className={styles.test}></div>
             </div>
         </div>
     )
