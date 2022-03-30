87dbbe517f9e30c03d3cf05a03fe086a9f00d3bb
philipp-moriss <filverem@gmail.com>
2022-03-30 22:32:45 +0300

    add: add modal and portal

--- src/Pages/GamePage/GamePage.tsx
+++ src/Pages/GamePage/GamePage.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './GamePage.module.scss'
 import './slider.css'
 import { Connection } from './Connection/Connection'
 import { v4 } from 'uuid'
 import imgCard from '../../Components/ConnectionCard/mock/card-img.jpg'
 import { ConnectionTypeCard } from '../../Components/ConnectionCard/ConnectionCard'
+import { DisconnectAllModal } from '../../Components/Modals/DisconnectAllModal'
 
 export const GamePage: React.FC<GamePropsType> = () => {
     const arrayConnectionsCreate: Array<CardType> = [
@@ -65,6 +66,18 @@ export const GamePage: React.FC<GamePropsType> = () => {
         },
     ])
 
+    const [showDisconnectModal, setShowDisconnectModal] =
+        useState<boolean>(false)
+
+    const openDisconnectModal = () => {
+        setShowDisconnectModal(true)
+    }
+
+    const closeDisconnectModal = () => {
+        setShowDisconnectModal(false)
+        document.body.style.overflow = 'auto'
+    }
+
     const callbackCard = (id: string, type: ConnectionTypeCard): void => {
         switch (type) {
             case 'createGame': {
@@ -110,30 +123,38 @@ export const GamePage: React.FC<GamePropsType> = () => {
                 type: 'newConnection',
             },
         ])
+        closeDisconnectModal()
     }
 
     return (
-        <div className={styles['wrapper']}>
-            <div className={styles['load-connection-box']}>
-                <Connection
-                    disconnectAllCallback={disconnectAllCallback}
-                    callback={callbackCard}
-                    disconnectGameCallback={disconnectGameCallback}
-                    arrayConnections={arrayConnectionsLoad}
-                    title={'Load connection'}
-                    type={'Load'}
-                />
+        <>
+            <div className={styles['wrapper']}>
+                <div className={styles['load-connection-box']}>
+                    <Connection
+                        disconnectAllCallback={openDisconnectModal}
+                        callback={callbackCard}
+                        disconnectGameCallback={disconnectGameCallback}
+                        arrayConnections={arrayConnectionsLoad}
+                        title={'Load connection'}
+                        type={'Load'}
+                    />
+                </div>
+                <div className={styles['create-connection-box']}>
+                    <Connection
+                        callback={callbackCard}
+                        disconnectGameCallback={disconnectGameCallback}
+                        arrayConnections={arrayConnectionsCreate}
+                        title={'Create connection'}
+                        type={'Create'}
+                    />
+                </div>
             </div>
-            <div className={styles['create-connection-box']}>
-                <Connection
-                    callback={callbackCard}
-                    disconnectGameCallback={disconnectGameCallback}
-                    arrayConnections={arrayConnectionsCreate}
-                    title={'Create connection'}
-                    type={'Create'}
-                />
-            </div>
-        </div>
+            <DisconnectAllModal
+                show={showDisconnectModal}
+                callBack={disconnectAllCallback}
+                closeCallback={closeDisconnectModal}
+            />
+        </>
     )
 }
 
