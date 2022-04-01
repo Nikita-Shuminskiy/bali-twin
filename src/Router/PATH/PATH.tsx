9af7a2d1dbe7ac7b1efb07ef3eb62d521bb2b1b8
philipp-moriss <filverem@gmail.com>
2022-04-01 13:19:02 +0300

    fix : pick menu style

--- src/Router/PATH/PATH.tsx
+++ src/Router/PATH/PATH.tsx
@@ -1,5 +1,11 @@
-export const PATH = {
+export const PATH: pathType = {
     HOME: '/',
     GAME: '/game',
     MAIN: 'bali-twin',
+}
+
+export type pathType = {
+    HOME: '/'
+    GAME: '/game'
+    MAIN: 'bali-twin'
 }
