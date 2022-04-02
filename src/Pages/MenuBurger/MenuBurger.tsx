f57d92e8db372184ce5d733a21d4c1924d815382
philipp-moriss <filverem@gmail.com>
2022-04-02 16:35:05 +0300

    fix: burger menu

--- src/Pages/MenuBurger/MenuBurger.tsx
+++ src/Pages/MenuBurger/MenuBurger.tsx
@@ -14,6 +14,10 @@ export const MenuBurger = () => {
         setShow(false)
     }
 
+    const connectWalletHandler = () => {
+        console.log('connectWalletHandler')
+    }
+
     return (
         <div className={`hamburger-menu frosted-glass`}>
             <label
@@ -23,7 +27,7 @@ export const MenuBurger = () => {
                 <span></span>
             </label>
             <ul className="menu__box frosted-glass">
-                <li>
+                <li className={'menu__item-inner'}>
                     <CustomLink
                         className={'menu__item'}
                         onClick={closeMenu}
@@ -31,31 +35,45 @@ export const MenuBurger = () => {
                     >
                         Game
                     </CustomLink>
+                    <div className={'menu__item-inner-pink-layout'}></div>
+                    <div className={'menu__item-inner-white-layout'}></div>
                 </li>
-                <li>
+                <li className={'menu__item-inner'}>
                     <CustomLink className="menu__item" to="#">
                         Collection
                     </CustomLink>
+                    <div className={'menu__item-inner-pink-layout'}></div>
+                    <div className={'menu__item-inner-white-layout'}></div>
                 </li>
-                <li>
+                <li className={'menu__item-inner'}>
                     <CustomLink className="menu__item" to="#">
                         Marketplace
                     </CustomLink>
+                    <div className={'menu__item-inner-pink-layout'}></div>
+                    <div className={'menu__item-inner-white-layout'}></div>
                 </li>
-                <li>
+                <li className={'menu__item-inner'}>
                     <CustomLink className="menu__item" to="#">
                         Jobs
                     </CustomLink>
+                    <div className={'menu__item-inner-pink-layout'}></div>
+                    <div className={'menu__item-inner-white-layout'}></div>
                 </li>
-                <li>
+                <li className={'menu__item-inner'}>
                     <CustomLink className="menu__item" to="#">
                         Docs
                     </CustomLink>
+                    <div className={'menu__item-inner-pink-layout'}></div>
+                    <div className={'menu__item-inner-white-layout'}></div>
                 </li>
-                <div className="menu__block-btn">
-                    <CustomLink to="#" className="btn">
+
+                <div
+                    onClick={connectWalletHandler}
+                    className={'btn-connect-wallet-mobile'}
+                >
+                    <span className={'btn-connect-wallet-mobile-text'}>
                         Connect wallet
-                    </CustomLink>
+                    </span>
                 </div>
             </ul>
         </div>
--- src/Router/RoutMap.tsx
+++ src/Router/RoutMap.tsx
@@ -4,13 +4,14 @@ import { AppHeader } from '../Pages/AppHeader/AppHeader'
 import { GamePage } from '../Pages/GamePage/GamePage'
 import { HomePage } from '../Pages/HomePage/HomePage'
 import { PATH } from './PATH/PATH'
+import { MenuBurger } from '../Pages/MenuBurger/MenuBurger'
 
 const RoutMap = () => {
     return (
         <>
             <AppHeader />
             <Routes>
-                <Route path={PATH.HOME} element={<HomePage />} />
+                <Route path={PATH.HOME} element={<MenuBurger />} />
                 <Route path={PATH.MAIN || PATH.HOME} element={<HomePage />} />
                 <Route
                     path={PATH.GAME}
