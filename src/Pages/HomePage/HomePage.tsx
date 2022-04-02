f9218d91773a87f0095d442a26aacc34911b91b8
philipp-moriss <filverem@gmail.com>
2022-04-02 18:00:41 +0300

    fix styles

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -7,10 +7,10 @@ export const HomePage = () => {
     return (
         <div className={styles['container']}>
             <div className={styles['logo-block']}>
-                <div>
+                <div className={styles['logo-block-btn-logo-inner']}>
                     <img src={logoHome} alt="logo-home" />
+                    <PlayGameButton />
                 </div>
-                <PlayGameButton />
                 <div className={styles['description']}>
                     Unite the world to create and develop the Island together
                 </div>
